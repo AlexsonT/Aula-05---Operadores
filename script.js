9eafcfc0da0858126c15a0263455033b6a715f88
Alexson Theobald <alexson.bass@gmail.com>
2021-01-03 12:50:58 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,83 @@
+// >>1. Crie uma variável mesmoNome que verifica se nomeDoFulano é igual a nomeDoBeltrano; 
+
+// nomeDoFulano = ("Thiago")
+// nomeDoBeltrano = ("Augusto")
+
+// mesmoNome = (nomeDoFulano == nomeDoBeltrano)
+
+// console.log(mesmoNome)
+// Testado Ok
+
+
+
+// >>2 .Crie uma variável maiorDeIdade que verifica se idade é de um adulto;
+
+// idadeA = 18 
+// idadeB = 17
+// idadeC = 25
+// maiorDeIdade1 = "Uma pessoa com " + idadeA + " anos já é adulto?" 
+// maiorDeIdade2 = idadeA >=18  
+
+// console.log(maiorDeIdade1, maiorDeIdade2 )
+// Testado Ok
+
+
+
+// >>3. Crie uma variável valorComJuros que calcula o valor de um boleto acrescido de 10% de juros por atraso;
+
+// ValorDoBoleto = 5000.00
+// ValorDoJuros = 1.10
+
+// valorComJuros = ValorDoBoleto * ValorDoJuros
+
+// console.log (valorComJuros)
+// Testado Ok
+
+
+
+// >>4.Faça a representação da fórmula de média aritmética, considerando um conjunto de elementos com 5 elementos nomeados como item1, item2, item3, item4 e item5;
+
+// item1 = 50
+// item2 = 20
+// item3 = 38
+// item4 = 68
+// item5 = 54
+
+// media = (item1 + item2 + item3 + item4 + item5)/5
+// console.log(media)
+// Testado Ok
+
+
+
+// >>5. O lucro bruto de uma empresa é representado pela receita líquida de vendas menos o custo dos produtos vendidos. A margem bruta de uma empresa é calculada pela divisão do lucro bruto pela receita líquida de vendas. Faça a representação da fórmula da margem bruta, multiplicando o resultado final por 100 para obter o valor percentual;
+
+// receitaLiquida = 10000
+// cPv = 8000
+// lucroBruto = receitaLiquida - cPv
+
+// margemBruta = lucroBruto/receitaLiquida
+
+// console.log(lucroBruto/receitaLiquida*100 + "%")
+// // Testado Ok
+
+
+
+// >>6. Usando qualquer ferramenta de codificação, crie um algoritmo seguindo os passos abaixo para encontrar o valor do saldo ao final do processo (atente-se para utilizar . como separador decimal):
+
+// O saldo da sua conta é R$ 1000,00
+// Você decide comprar uma calça por R$ 99,90
+// Você recebe o seu salário de R$ 2500,00
+// Você está caminhando na rua e decide dar R$ 0,10 para um morador de rua
+// Você compra um ar condicionado cujo valor é 25% do seu saldo atual
+// Você decide antecipar o pagamento de uma dívida usando metade do seu saldo
+// Qual o valor do seu saldo? 🤔
+
+// saldo = 1000.00
+// saldo = saldo - 99.90
+// saldo = saldo + 2500.00
+// saldo = saldo - 0.10
+// saldo = saldo * 0.75
+// saldo = saldo * 0.50
+
+// console.log(saldo)
+// // Testado Ok
